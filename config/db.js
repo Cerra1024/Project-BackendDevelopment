e9552626597a752efcfa8b457a7b4d4e7653fa0a
Cerra1024 <sangarihar@gmail.com>
2026-06-02 22:15:55 -0400

    Set up MongoDB database connection

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error("CRITICAL ERROR: MONGO_URI is missing from environmental variables.");
+    process.exit(1);
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    
+    console.log(`✨ MongoDB Connected Successfully: ${conn.connection.host}`);
+  } catch (error) {
+
+    console.error("MongoDB connection error occured.");
+    console.error(`Error Details: ${error.message}`);
+    
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
