77c647ecd58f18619e7c9e04ec64f30746ed609e
Cerra1024 <sangarihar@gmail.com>
2026-06-03 05:09:28 -0400

    created task creation route

---
+++ routes/taskRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Task = require("../models/Task");
+const Project = require("../models/Project");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+// Create task in a project
+router.post("/projects/:projectId/tasks", protect, async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.projectId);
+
+    if (!project) {
+      return res.status(404).json({
+        message: "Project not found",
+      });
+    }
+
+    // Ownership check
+    if (project.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        message: "Not authorized",
+      });
+    }
+
+    const { title, description, status } = req.body;
+
+    const task = await Task.create({
+      title,
+      description,
+      status,
+      project: project._id,
+    });
+
+    res.status(201).json(task);
+  } catch (error) {
+    console.error("CREATE TASK ERROR:");
+    console.error(error);
+
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
